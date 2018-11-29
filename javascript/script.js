4712034b5ba1ab9443a8279696cc2811c4c9e010
SachaFolcke <folcke.sacha@gmail.com>
2018-11-29 08:57:07 +0100

    1ere mise en ligne

---
+++ javascript/script.js
@@ -0,0 +1,49 @@
+function showMaster() {
+    document.getElementById("info-master").style.display = "block"
+    document.getElementById("blackBackground").style.display = "block";
+}
+
+function showSite() {
+    document.getElementById("info-site").style.display = "block"
+    document.getElementById("blackBackground").style.display = "block";
+}
+
+function show2048() {
+    document.getElementById("info-2048").style.display = "block"
+    document.getElementById("blackBackground").style.display = "block";
+}
+
+function showRPG() {
+    document.getElementById("info-rpg").style.display = "block"
+    document.getElementById("blackBackground").style.display = "block";
+}
+function hideInfo() {
+    document.getElementById("blackBackground").style.display = "none";
+    document.getElementById("info-master").style.display = "none";
+    document.getElementById("info-site").style.display = "none";
+    document.getElementById("info-2048").style.display = "none";
+    document.getElementById("info-rpg").style.display = "none";
+}
+
+
+function next(className) {
+    var elements = document.getElementsByClassName(className);
+    var done = false;
+
+    for(i = 0; i < elements.length -1; i++) {
+        if(elements[i].classList.contains("visible")) {
+            elements[i].classList.remove("visible");
+            elements[i].classList.add("invisible");
+            elements[i+1].classList.remove("invisible");
+            elements[i+1].classList.add("visible");
+            done = true;
+            break;
+        }
+    }
+    if(!done) {
+        elements[elements.length-1].classList.remove("visible");
+        elements[elements.length-1].classList.add("invisible");
+        elements[0].classList.remove("invisible");
+        elements[0].classList.add("visible")
+    }
+}
