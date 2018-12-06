183d587c073839e09f97e23dad221997ff75f8c9
SachaFolcke <folcke.sacha@gmail.com>
2018-12-06 11:53:12 +0100

    Début optimisation js

--- javascript/script.js
+++ javascript/script.js
@@ -1,38 +1,34 @@
-function showMaster() {
-    document.getElementById("info-master").style.display = "block";
-    document.getElementById("blackBackground").style.display = "block";
+function showProject(id) {
+    showBlackBackground();
+    document.getElementById(id).style.display = "block";
 }
 
-function showSite() {
-    document.getElementById("info-site").style.display = "block";
-    document.getElementById("blackBackground").style.display = "block";
-}
 
-function show2048() {
-    document.getElementById("info-2048").style.display = "block";
-    document.getElementById("blackBackground").style.display = "block";
-}
-
-function showRPG() {
-    document.getElementById("info-rpg").style.display = "block";
-    document.getElementById("blackBackground").style.display = "block";
-}
-
-function hideInfo() {
-    document.getElementById("blackBackground").style.display = "none";
-    document.getElementById("info-master").style.display = "none";
-    document.getElementById("info-site").style.display = "none";
-    document.getElementById("info-2048").style.display = "none";
-    document.getElementById("info-rpg").style.display = "none";
+function hideProject() {
+    hideBlackBackground();
+    var projects = document.getElementById("all-projects").getElementsByTagName("div");
+    for(var i = 0; i < projects.length; i++){
+        projects[i].style.display = "none";
+    }
     document.getElementById("mobile-nav-overlay").style.display = "none";
 }
 
-function showMobileMenu() {
-    document.getElementById("mobile-nav-overlay").style.display = "flex";
+
+
+function showBlackBackground() {
     document.getElementById("blackBackground").style.display = "block";
 }
 
-function next(className) {
+function hideBlackBackground() {
+    document.getElementById("blackBackground").style.display = "none";
+}
+
+function showMobileMenu() {
+    showBlackBackground();
+    document.getElementById("mobile-nav-overlay").style.display = "flex";
+}
+
+function nextPhoto(className) {
     var elements = document.getElementsByClassName(className);
     var done = false;
 
